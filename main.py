ab1c7a4c294eb76be8170090415d2d4fba00bb2a
Pavel <pavel.fabian05@gmail.com>
2023-11-13 15:30:28 +0100

    reading and printing redid with pandas, added requirements.txt

--- main.py
+++ main.py
@@ -1,39 +1,21 @@
-import csv
-import json
+import pandas as pd
 
 from html_table import gen_html
 import calc
 
 
-def load_elements_from_json(json_file):
-    with open(json_file, 'r', encoding='utf-8') as file:
-        groups = json.load(file)
-    return groups
-
-def load_elements_from_csv(csv_file):
-    elements = []
-    with open(csv_file, 'r', encoding="utf-8") as file:
-        csv_reader = csv.reader(file)
-        for row in csv_reader:
-            elements.append(row)
-    return elements
-
-
 def search(elements, phrase):
-    found = []
-    for element in elements:
-        if phrase.lower() == element[1].lower() or phrase.lower() == element[2].lower():
-            found.append(element)
-    return found
+    condition = (elements['Element'] == phrase) | (elements['Symbol'] == phrase)
+    return elements[condition]
 
-
-def display(elements):
-    for element in elements:
-        print(element)
 
 def main():
-    elements = load_elements_from_csv('elements.csv')
-    groups = load_elements_from_json('groups.json')
+    pd.set_option('display.max_rows', None)
+    pd.set_option('display.max_columns', None)
+    pd.set_option('display.width', None)
+    pd.set_option('display.max_colwidth', None)
+    elements = pd.read_csv('elements.csv')
+    groups = pd.read_json('groups.json')
 
     while True:
         print("1. Display all elements")
@@ -49,10 +31,10 @@ def main():
             break
         elif choice == "1":
             print("displaying all elements: ")
-            display(elements)
+            print(elements)
         elif choice == "2":
             print("displaying all groups: ")
-            display(groups)
+            print(groups)
         elif choice == "3":
             print("created html table ")
             gen_html("elements.csv", 'periodic_table.html')
@@ -61,11 +43,11 @@ def main():
         elif choice == "10":
             phrase = input("Enter name or symbol of element to search for: ")
             found = search(elements, phrase)
-            if found:
-                print(elements[0])
-                display(found)
+            if not found.empty:
+                print("Element found:")
+                print(found)
             else:
-                print("Element not found. Please try again.")
+                print("Element not found.")
         else:
             print("Invalid choice. Please try again.")
 
